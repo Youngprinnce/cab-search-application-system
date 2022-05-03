19282345f501c3343429e06d04e3c6223a5594b8
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-05-03 16:03:12 +0100

    structure driver model

---
+++ src/api/models/driver.models.ts
@@ -0,0 +1,62 @@
+/* eslint-disable no-underscore-dangle */
+import mongoose, { Schema } from 'mongoose';
+
+interface DriverModel extends mongoose.Document {
+    name: string;
+    email: string;
+    phone_number: number;
+    license_number: string;
+    car_number: string;
+    isVerified: boolean;
+    id: number;
+}
+
+const DriverSchema: Schema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone_number: {
+    type: Number,
+    required: true,
+    unique: true,
+    min: 1000000000,
+    max: 9999999999,
+  },
+  license_number: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  car_number: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+}, {
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+});
+
+DriverSchema.method('toJSON', function () {
+  const { ...object } = this.toObject();
+  delete object._id;
+  delete object.__v;
+  return object;
+});
+
+export default mongoose.model<DriverModel>('Driver', DriverSchema);
