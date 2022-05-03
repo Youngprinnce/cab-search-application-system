b62ba4a4015648bddfebe457bb46f3e8e931266d
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-05-03 17:00:14 +0100

    document apis using swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express, { Application, json, urlencoded } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 import { NODE_ENV, BASE_URL } from './config';
 import InitiateMongoServer from './config/db';
 
@@ -10,6 +12,36 @@ import driverRouter from './api/routes/driver.routes';
 import cabRouter from './api/routes/cab.routes';
 
 const app: Application = express();
+
+const swaggerDefinition = {
+  openapi: '3.0.3',
+  info: {
+    title: 'Cab Search Application System API documentation',
+    version: '1.0.0',
+    description: 'Cab Search API documentation',
+    contact: {
+      name: 'API Support',
+      email: 'ajiboyeadedotun16@gmail.com',
+    },
+  },
+  servers: [
+    {
+      url: `${BASE_URL}/api/v1`,
+      description: 'Development server',
+    },
+  ],
+};
+// options for the swagger docs
+const options = {
+  // import swaggerDefinitions
+  swaggerDefinition,
+  // path to the API docs
+  apis: ['./src/docs/*.yaml'],
+};
+// initialize swagger-jsdoc
+const swaggerSpec = swaggerJsDoc(options);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Initiate Database Connection
 InitiateMongoServer();
