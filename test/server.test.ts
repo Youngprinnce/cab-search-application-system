8b6dae61795bdd6352c8875c6b12a29b35f977b5
Ajiboye Adedotun <ajiboyeadedotun16@gmail.com>
2022-05-03 16:42:40 +0100

    server test passed

---
+++ test/server.test.ts
@@ -0,0 +1,23 @@
+import { describe, it } from 'mocha';
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../src/server';
+
+chai.use(chaiHttp);
+
+describe('Server', () => {
+  it('Should display Cab Search Application Backend Service API', (done) => {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).to.be.eql(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body.status).to.eql('success');
+        expect(res.body.message).to.eql(
+          'Cab Search Application Backend Service API',
+        );
+        done();
+      });
+  });
+});
